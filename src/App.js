6969711377ccafdb02360a1f16002db01e466b5c
Bohdan Tarasenko <bt.tarasenko@gmail.com>
2024-10-15 12:31:04 +0200

    create components and route setings and protected route setings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  return <div className="App"></div>
+    return (
+        <Router>
+            <Routes>
+                {/* Маршрут для домашней страницы, доступный всем */}
+                <Route path="/" element={<Home />} />
+
+                {/* Маршрут для страницы логина */}
+                <Route path="/login" element={<Login />} />
+
+                {/* Защищенный маршрут для профиля */}
+                <Route
+                    path="/profile"
+                    element={
+                        <ProtectedRoute>
+                            <Profile />
+                        </ProtectedRoute>
+                    }
+                />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
